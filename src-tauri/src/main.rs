5617a2dce1e0e93f93ecd580562021e462b45b45
Brenden <brenden.ferguson@gmail.com>
2022-03-25 17:07:31 -0700

    Introduce sysinfo crate + example

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,8 @@
   windows_subsystem = "windows"
 )]
 
+use sysinfo::{System, SystemExt};
+
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![my_custom_command])
@@ -12,5 +14,11 @@ fn main() {
 
 #[tauri::command]
 fn my_custom_command() {
-  println!("I was invoked from JS!");
+  let mut sys = System::new_all();
+
+// First we update all information of our `System` struct.
+sys.refresh_all();
+
+  // print all system information
+  println!("{:#?}", sys.processors());
 }
