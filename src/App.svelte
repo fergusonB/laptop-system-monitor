9bbf66d30d09e7bfceb1d3da0d418fc8cff5926c
Brenden <brenden.ferguson@gmail.com>
2022-04-28 16:29:18 -0700

    get-data-10

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,17 @@
 <script lang="ts">
-	//@ts-ignore
-	const invoke = window.__TAURI__.invoke
 	
-	export let name: string;
+	import Memory from './components/Memory.svelte'
 
-	invoke('total_memory').then(mem=>alert(mem))
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 
+	<Memory />
+
+	
 
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ src/components/Memory.svelte
@@ -0,0 +1,10 @@
+
+<script lang="ts">
+    //@ts-ignore
+    const invoke = window.__TAURI__.invoke
+        
+        invoke('total_memory').then(total_memory => alert(total_memory));
+
+</script>
+
+hello world
