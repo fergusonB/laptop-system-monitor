9fbd05250b51b38d958dbac142e3b17339164ca4
Brenden <brenden.ferguson@gmail.com>
2022-04-28 16:20:16 -0700

    get_memory

--- src-tauri/src/get_data.rs
+++ src-tauri/src/get_data.rs
@@ -7,3 +7,11 @@ use sysinfo::{System, SystemExt};
 pub fn total_memory() -> String{
   System::new_all().total_memory().to_string().into()
 }
+#[tauri::command]
+pub fn used_memory() -> String{
+  System::new_all().used_memory().to_string().into()
+}
+#[tauri::command]
+pub fn free_memory() -> String{
+  System::new_all().available_memory().to_string().into()
+}
