73984fe639ce943faa9bdd595bec285f9b81f2e0
Brenden <brenden.ferguson@gmail.com>
2022-04-28 17:12:02 -0700

    basic memory stats

--- src/components/Memory.svelte
+++ src/components/Memory.svelte
@@ -2,9 +2,27 @@
 <script lang="ts">
     //@ts-ignore
     const invoke = window.__TAURI__.invoke
-        
-        invoke('total_memory').then(total_memory => alert(total_memory));
+
+    import {onDestroy} from 'svelte'
+    
+    let total_memory = 'Loading...'
+    total_memory = invoke('total_memory').then(res=>total_memory=res)
+
+    let used_memory = 'Loading...'
+    
+    
+    // Looping
+    const interval = setInterval(async ()=>{
+
+        used_memory = await invoke('used_memory')
+
+    }, 1000);
+    onDestroy(() => clearInterval(interval));    
 
 </script>
 
-hello world
+<p>
+    Used Memory: {used_memory}
+    <br>
+    Total Memory: {total_memory}
+</p>
